78b484d876c07af6441b09f63f08e4b97c4f2a23
gmar7777 <gmaruzhenko@yahoo.ca>
2019-05-19 14:37:37 -0700

    inittal before pyro

--- brayton.py
+++ brayton.py
@@ -1,10 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyromat as pyro
+import math as math
 
 
+P_RATIO_MAX = 20        # P1/P2
+GAMMA = 1.4
+# Ambient
+T0 = 273.15 + 10        # KELVIN
+T_max = 273.15 + 1000   # KELVIN
+P0 = 101 * 1000  # Pa
+DELTA_P_RATIO = 1
+p_ratio_1 = [i * DELTA_P_RATIO for i in range(1, P_RATIO_MAX)]
+v_ratio_1 = [i ** (-GAMMA) for i in p_ratio_1]
+
+plt.plot(v_ratio_1, p_ratio_1)
+plt.show()
 
 
-PRESSURERATIO = 20
-
-AMBIENTTEMP = 273.15 + 10 # KELVIN
-AMBIENTPRESSURE = 101 * 1000 # Pa
-MAXTEMP = 1000 + 273.15 + 10 # KELVIN
 
